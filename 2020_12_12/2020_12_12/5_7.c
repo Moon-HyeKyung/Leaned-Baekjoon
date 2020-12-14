46369d8fc0fd8cc7ca7202e91521fc2afc26669b
Moon-HyeKyung <ansgprud02@gmail.com>
2020-12-14 23:34:41 +0900

    BaekJoon Test 5. Array

---
+++ 2020_12_12/2020_12_12/5_7.c
@@ -0,0 +1,38 @@
+/*
+문제
+대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+
+입력
+첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 
+점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+출력
+각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int c, n, sum = 0;
+	int score;
+	scanf_s("%d", &c);
+	for (int i = 0; i < c; i++) {
+		do {
+			scanf_s("%d", &n);
+		} while (n < 1 || n>1000);
+		
+		for (int j = 0; j < n; j++) {
+			scanf_s("%d", &score);
+			if (score < 0 || score > 100) {
+				j--; continue;
+			}
+			sum += score;
+		}
+		printf("%.3f%\n",(float)sum / n);
+		sum = 0;
+	}
+	return 0;
+}
